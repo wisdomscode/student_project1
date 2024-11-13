4ff755895597eca536cdac8f0aa560fbf21b0b66
Wisdom <wisdomscode@gmail.com>
2024-11-13 11:44:00 +0100

    Added create new student

--- students_app/views.py
+++ students_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from  .models import Student
-# Create your views here.
+from django.contrib import messages
+
 
 def homepage(request):
     students = Student.objects.all()
@@ -8,4 +9,32 @@ def homepage(request):
 
 
 def createStudent(request):
+    if request.method == 'POST':
+        firstName = request.POST.get('first_name')
+        lastName = request.POST.get('last_name')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        department = request.POST.get('department')
+
+        # check if all field are provided
+        if not all([firstName, lastName, age, gender, department]) :
+            messages.error(request, 'All fields are required')
+            return render(request, 'students/create_student.html')
+
+
+        try: 
+            # create the Student instance
+            Student.objects.create(
+                first_name=firstName, 
+                last_name=lastName,
+                age=age, 
+                gender=gender, 
+                department=department
+            )
+            return redirect('home_page')  # redirect to home page using name in url
+        
+        except Exception as e:
+            messages.error(request, f'An error occurred: {str(e)}')
+            return render(request, 'students/create_student.html')
+
     return render(request, 'students/create_student.html')
