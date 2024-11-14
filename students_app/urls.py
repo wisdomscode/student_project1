6c5d147342b77ede1d364cbaa4f503b7f0321fff
Wisdom <wisdomscode@gmail.com>
2024-11-14 11:54:53 +0100

    Update and delete

--- students_app/urls.py
+++ students_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home_page'),
     path('create', views.createStudent, name='createStudent'),
+    path('show/<int:id>', views.showStudent, name='showStudentDetails'),
+    path('edit/<int:id>', views.editStudent, name='editStudent'),
+    path('delete/<int:id>', views.deleteStudent, name='deleteStudent'),
 ]
--- students_app/views.py
+++ students_app/views.py
@@ -38,3 +38,46 @@ def createStudent(request):
             return render(request, 'students/create_student.html')
 
     return render(request, 'students/create_student.html')
+
+
+def showStudent(request, id):
+    student = Student.objects.get(id=id)
+    return render(request, 'students/show_student.html', {'student': student})
+
+
+def editStudent(request, id):
+    student = Student.objects.get(id=id)
+
+    # to update the student
+    if request.method == 'POST':
+        firstName = request.POST.get('first_name')
+        lastName = request.POST.get('last_name')
+        age = request.POST.get('age')
+        gender = request.POST.get('gender')
+        department = request.POST.get('department')
+
+        # check if all field are provided
+        if not all([firstName, lastName, age, gender, department]) :
+            messages.error(request, 'All fields are required')
+            return render(request, 'students/edit_student.html')
+        
+        student.first_name = firstName
+        student.last_name = lastName
+        student.age = age
+        student.gender = gender
+        student.department = department
+
+        student.save()
+        return redirect('showStudentDetails', student.id)
+
+
+    return render(request, 'students/edit_student.html', {'student': student})
+
+
+def deleteStudent(request, id):
+    student = Student.objects.get(id=id)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('home_page')
+
+    return render(request, 'students/delete_student.html', {'student': student})
